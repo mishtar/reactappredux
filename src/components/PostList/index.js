93be30dee1ccdbe0b8d9ddded9239c8b70effe1c
Sebastian Arboleda <SArboleda@arrivelogistics.com>
2018-04-11 14:03:00 -0500

    Create a React app and publish it to GitHub Pages

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -28,8 +28,8 @@ class PostList extends Component {
         return(
             <div>
                 Orden:{' '}
-                <Button color="info" className="post-list-button" active={this.state.order===1} onClick={() => this.handleClick(1)} outline><div>Ascendente</div></Button>{' '}
-                <Button color="primary" className="post-list-button" active={this.state.order===0} onClick={() => this.handleClick(0)}><div>Descendente</div></Button>{' '}
+                <Button color="primary" className="post-list-button" active={this.state.order===1} onClick={() => this.handleClick(1)} outline><div>Ascendente</div></Button>{' '}
+                <Button color="primary" className="post-list-button" active={this.state.order===0} onClick={() => this.handleClick(0)} outline><div>Descendente</div></Button>{' '}
                 {this.state.posts
                     .sort(this.state.order ? (a, b) => parseInt(a.votes, 10) - parseInt(b.votes, 10) : (a, b) => parseInt(b.votes, 10) - parseInt(a.votes, 10))
                     .map((element) => (
